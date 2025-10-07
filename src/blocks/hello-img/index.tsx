17d3ee4b9e687fbaa7a8b8c9b589b72213d33ccb
Daniel Vyakulin <danya@danya-lapka.(none)>
2025-10-07 21:31:48 +0400

    meowing

--- src/blocks/hello-img/index.tsx
+++ src/blocks/hello-img/index.tsx
@@ -1,21 +1,37 @@
 'use client';
 import Image from "next/image";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import styles from './style.module.scss';
 import clsx from "clsx";
+import { useSound } from "@/hooks";
 
 const Hello_Img = () => {
-  const [state, setState] = useState(false);
-
   const isProd = process.env.NODE_ENV === 'production';
-
+  const [state, setState] = useState(false);
   let url = state ? '/hello-img2': '/hello-img';
+  const { start, stop } = useSound(`${isProd ? '/lapka' : ''}/sound/meow.mp3`, 1200);
+
+  const meowing = () => {
+    setState(true); 
+    start();
+  }
+  const stopMeowing = () => {
+    setState(false); 
+    stop();
+  }
 
   return (
-    <Image src={`${isProd ? '/lapka' : ''}${url}.png`} alt={url} onClick={() => { setState(!state); }} fill className={clsx({
-      [`image c-pointer`]: true,
-      [styles[`image`]]: true
-    })}/>
+    <Fragment>
+      <Image src={`${isProd ? '/lapka' : ''}${url}.png`} alt={url} fill className={clsx({
+        [`image c-pointer`]: true,
+        [styles[`image`]]: true
+      })}
+      onMouseDown={meowing}
+      onMouseUp={stopMeowing}
+      onMouseLeave={stopMeowing}
+      onTouchStart={meowing}
+      onTouchEnd={stopMeowing}/>
+    </Fragment>
   )
 }
 
---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+import { useSound } from "./useSound";
+
+export { useSound }
