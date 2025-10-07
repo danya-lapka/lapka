475f206bbc49a778b54b61a1c11f609ab0ffd205
Daniel Vyakulin <danya@danya-lapka.(none)>
2025-10-07 19:42:56 +0400

    конченный что ли

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   basePath: '/lapka',
-  assetPrefix: '/lapka/',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  /* config options here */
 };
 
 export default nextConfig;
