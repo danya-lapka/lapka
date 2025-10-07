873ac2b4a8f7a1927ec69b41125fba1b1046a52b
Daniel Vyakulin <danya@danya-lapka.(none)>
2025-10-07 19:58:37 +0400

    fix23323

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  
-};
-
 const isProd = process.env.NODE_ENV === 'production';
 
-  module.exports = {
-    basePath: isProd ? '/lapka' : '', // Замените 'project' на название вашего репозитория
-    assetPrefix: isProd ? '/lapka/' : '', // Для статических ресурсов
-    trailingSlash: true, // Опционально: добавляет слеши к URL
-  };
+const nextConfig: NextConfig = {
+  basePath: isProd ? '/lapka' : '', // Замените 'project' на название вашего репозитория
+  assetPrefix: isProd ? '/lapka/' : '', // Для статических ресурсов
+  trailingSlash: true, // Опционально: добавляет слеши к URL
+};
 
 export default nextConfig;
