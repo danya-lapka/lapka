e962ec899492485100677b60ede6a5de8156a040
Daniel Vyakulin <danya@danya-lapka.(none)>
2025-10-07 19:39:38 +0400

    заебал

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   basePath: '/lapka',
+  assetPrefix: '/lapka/',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
