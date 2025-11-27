36455fbf4ef2960aa44746230b5afc8ccc105a5a
Daniel Vyakulin <danya@danya-lapka.(none)>
2025-11-27 04:40:20 +0400

    finally

--- packages/css-vite/src/index.ts
+++ packages/css-vite/src/index.ts
@@ -4,4 +4,4 @@ import atomicCssPlugin from './plugin';
 export default atomicCssPlugin;
 
 // Также можно экспортировать функцию генерации, если она вдруг понадобится отдельно
-export { generateCss, THEME_COLORS, PSEUDO_VARIANTS, BREAKPOINTS } from './engine';
+export { generateCss, THEME_COLORS, PSEUDO_VARIANTS, BREAKPOINTS, CssDeclaration, CssRule, escapeClassName } from './engine';
